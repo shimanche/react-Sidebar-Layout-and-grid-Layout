212603ba65aa001eca200c9c0f9b469e3096e5ff
shimanche <shimanche.sute@gmail.com>
2024-04-07 19:05:38 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
+        {/* 左側のコンテンツ */}
+        <div className="flex flex-col text-white overflow-hidden">
+          <div className="bg-slate-800 px-2">this</div>
+          <div className="bg-blue-500 text-white p-4">
+            This is an example component with Tailwind CSS!
+          </div>
+        </div>
+        {/* 右側のコンテンツ */}
+        <div className="bg-gray-700">
+          <div className="grid grid-cols-[300px_1fr] gap-4 m-10">
+            {/* 左側の列 */}
+            <div className="image">画像１</div>
+            {/* 右側の列 */}
+            <div className="flex flex-col justify-between">
+              <div className="name">名前</div>
+              <div className="image">画像２</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
